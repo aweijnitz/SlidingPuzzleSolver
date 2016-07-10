2e3b974ad6db2a4ab4ba3ac4e3e12ff07b4153a7
Anders Weijnitz <anders.weijnitz@gmail.com>
2016-07-10 20:51:16 +0200

    Added distance function

--- test/test_distance.js
+++ test/test_distance.js
@@ -5,13 +5,51 @@ var board = require('../lib/board.js').board;
 // Function under test
 var distance = require('../lib/distance.js');
 
-test('distance function', function (t) {
+test('distance function Y delta', function (t) {
 
-  var arr0 = [0, 1, 2];
-  var arr1 = [1, -1, 2];
-  t.equal(3, distance(arr0, arr1));
+  var arr0 =
+    [3, 1, 2,
+      0, 4, 5,
+      6, 7, 8];
+  var arr1 =
+    [0, 1, 2,
+      3, 4, 5,
+      6, 7, 8];
+
+  t.equal(distance(arr0, arr1), 2);
   t.end();
 });
+
+test('distance function X delta to sorted board', function (t) {
+
+  var arr0 =
+    [1, 0, 2,
+      3, 4, 5,
+      6, 7, 8];
+  var arr1 =
+    [0, 1, 2,
+      3, 4, 5,
+      6, 7, 8];
+
+  t.equal(distance(arr0, arr1), 2);
+  t.end();
+});
+
+test('distance function X and Y delta', function (t) {
+
+  var arr0 =
+    [1, 4, 2,
+      3, 0, 5,
+      6, 7, 8];
+  var arr1 =
+    [0, 1, 2,
+      3, 4, 5,
+      6, 7, 8];
+
+  t.equal(distance(arr0, arr1), 4);
+  t.end();
+});
+
 
 test('distance function on two identical boards', function (t) {
 
