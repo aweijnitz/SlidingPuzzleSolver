c523e0c69a646d11ffc925d4a7a19f6842436a59
Anders Weijnitz <anders.weijnitz@gmail.com>
2016-07-10 16:30:28 +0200

    Refactoring done. Tests passing.

--- test/test_board.js
+++ test/test_board.js
@@ -1,14 +1,14 @@
 var test = require('tape');
 
-var up = require('../lib/moves.js').up;
-var down = require('../lib/moves.js').down;
-var left = require('../lib/moves.js').left;
-var right = require('../lib/moves.js').right;
-
 // Function under test
 var board = require('../lib/board.js').board;
 var shuffle = require('../lib/board.js').shuffle;
 var possibleMoves = require('../lib/board.js').possibleMoves;
+
+var up = require('../lib/board.js').up;
+var down = require('../lib/board.js').down;
+var left = require('../lib/board.js').left;
+var right = require('../lib/board.js').right;
 
 
 test('board function', function (t) {
@@ -48,3 +48,43 @@ test('shuffle function', function (t) {
 });
 
 
+
+test('up function', function (t) {
+  var dim = 3;
+  var b = board(dim);
+  t.deepEqual(up(down(b)),
+    [0, 1, 2,
+      3, 4, 5,
+      6, 7, 8]);
+  t.end();
+});
+
+test('down function', function (t) {
+  var dim = 3;
+  var b = board(dim);
+  t.deepEqual(down(b),
+    [3, 1, 2,
+      0, 4, 5,
+      6, 7, 8]);
+  t.end();
+});
+
+test('left function', function (t) {
+  var dim = 3;
+  var b = board(dim);
+  t.deepEqual(left(right(b)),
+    [0, 1, 2,
+      3, 4, 5,
+      6, 7, 8]);
+  t.end();
+});
+
+test('right function', function (t) {
+  var dim = 3;
+  var b = board(dim);
+  t.deepEqual(right(b),
+    [1, 0, 2,
+      3, 4, 5,
+      6, 7, 8]);
+  t.end();
+});
